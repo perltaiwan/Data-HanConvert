2c6c8ea7ffe15f642b8ab2bd241092c0625ac547
Kang-min Liu <gugod@gugod.org>
2014-10-01 16:26:17 +0200

    split the old "characters.txt" into multiple files.
    We simply exclude many conversions because the content
    of "traditional_characters.txt" also contains some simplified
    characters.
    
    This change builds 3 conversions table follows the descriptions
    of Unihan_Variants.txt from:
    
        http://www.unicode.org/reports/tr38/#SCTC
    
    We also produce a full list of characters that has no variants, which
    means they are just use as-is in both traditional and simplified
    Chinese. (case 1 under 3.7.1 of the abovementioned URL.)

--- dev-bin/unihan2txt.pl
+++ dev-bin/unihan2txt.pl
@@ -3,15 +3,16 @@
 =head1 unihan2txt.pl
 
 This program converts and extracts the tradition/simplified mapping from
-Unihan_Variants.txt to the text mapping file named "characters.txt" with format
-described in README.md. The order in the file is sorted by the unicode codepoint
-of the first column.
+Unihan_Variants.txt to the text mapping files named "characters_invariant.txt",
+"characters_multivariant.txt", and "characters_singlevariant.txt".  The format
+is described in README.md.
 
 Unihan_Variants.txt can be found from the Unihan.zip distribution at
 L<http://www.unicode.org/Public/UNIDATA/Unihan.zip>
 
 =cut
 
+use v5.14;
 use strict;
 use warnings;
 use utf8;
@@ -19,42 +20,78 @@ use FindBin;
 use IO::All;
 use Cwd qw(realpath);
 use JSON qw(encode_json);
+use YAML;
+use Unicode::UCD 'charscript';
 
 sub u { chr(hex(substr($_[0],2))) }
 
 my $uv_path = shift @ARGV or die "Usage: $FindBin::Script /path/of/Unihan_Variants.txt\n";
 my $uv_io = io($uv_path)->utf8->chomp;
 
-my $dist_root = realpath( io->catdir($FindBin::Bin, "..") );
-my $char_io = io->catfile($dist_root, "src", "characters.txt")->utf8->assert;
-
-$char_io->unlink if $char_io->exists;
-$char_io->println("# Generated with unihan2txt.pl");
-
-my %mappings;
+my %variants;
 while(defined(my $line = $uv_io->getline)) {
     next if $line =~ /^#/;
     next if $line =~ /^\s*$/;
     my ($char, $type, @variants) = split /\s+/o, $line;
     next unless $type =~ /^(kSimplifiedVariant|kTraditionalVariant)$/o;
 
-    if ($type eq "kSimplifiedVariant") {
-        for (@variants) {
-            $mappings{ u($char) . " " . u($_) } = 1;
-        }
+    ($char, @variants) = map { u($_) } ($char, @variants);
+
+    for (@variants) {
+        $variants{$type}{$char}{$_}++;
     }
-    else {
-        for (@variants) {
-            $mappings{ u($_) . " " . u($char) } = 1;
+}
+
+# Cases in http://www.unicode.org/reports/tr38/#SCTC
+# Taken from: https://en.wikipedia.org/wiki/Han_unification#Unicode_ranges
+my $cjk_ideograph_ranges = [
+    [0x4E00,  0x9FFF],
+    [0x3400,  0x4DBF],
+    [0x20000, 0x2A6DF],
+    [0x2A700, 0x2B73F],
+    [0x2B740, 0x2B81F],
+];
+
+my $dist_root = realpath( io->catdir($FindBin::Bin, "..") );
+my $char_invariant_io = io->catfile($dist_root, "src", "characters_invariant.txt")->utf8->assert;
+my $char_multivariant_io = io->catfile($dist_root, "src", "characters_multivariant.txt")->utf8->assert;
+my $char_singlevariant_io = io->catfile($dist_root, "src", "characters_singlevariant.txt")->utf8->assert;
+
+my %mappings_singlevariant;
+my %mappings_multivariant;
+
+for my $range (@$cjk_ideograph_ranges) {
+    for (my $charcode = $range->[0]; $charcode <= $range->[1]; $charcode++) {
+        my $c = chr($charcode);
+        if ( !(exists $variants{kSimplifiedVariant}{$c}) && !(exists $variants{kTraditionalVariant}{$c}) ) {
+            $char_invariant_io->println($c);
+        } elsif ((exists $variants{kSimplifiedVariant}{$c}) && !(exists $variants{kTraditionalVariant}{$c})) {
+            my @d = keys %{$variants{kSimplifiedVariant}{$c}};
+            if (@d == 1) {
+                $mappings_singlevariant{"$c $d[0]"}++;
+            } else {
+                my $d = join "," => @d;
+                $mappings_multivariant{"$c $d"}++;
+            }
+        } elsif (!(exists $variants{kSimplifiedVariant}{$c}) && (exists $variants{kTraditionalVariant}{$c})) {
+            my @d = keys %{$variants{kTraditionalVariant}{$c}};
+            if (@d == 1) {
+                $mappings_singlevariant{"$d[0] $c"}++;
+            } else {
+                my $d = join "," => @d;
+                $mappings_multivariant{"$d $c"}++;
+            }
+        } elsif ((exists $variants{kSimplifiedVariant}{$c}) && (exists $variants{kTraditionalVariant}{$c})) {
+            if (exists $variants{kSimplifiedVariant}{$c}{$c}) {
+            } else {
+            }
         }
     }
 }
 
-my %traditional_characters;
-@traditional_characters{ split(/\s/, io("src/traditional_characters.txt")->utf8->all) } = ();
-
-for my $mapping (sort keys %mappings) {
-    my ($t, $s) = split /\s/, $mapping;
-    next if exists $traditional_characters{$s};
-    $char_io->println($mapping);
+for (keys %mappings_singlevariant) {
+    $char_singlevariant_io->println($_);
+}
+for (keys %mappings_multivariant) {
+    $char_multivariant_io->println($_);
 }
